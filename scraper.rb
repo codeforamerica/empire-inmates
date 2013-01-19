28c9ec9dd231253ce187ada7d0511237fbbafef4
CJ Bryan <christianbryan@gmail.com>
2013-01-18 17:13:21 -0800

    Now parses out a page into a nice hash of data. Added a simple request loop, though I think we're getting throttled.

--- scraper.rb
+++ scraper.rb
@@ -10,16 +10,47 @@ end
 
 class Scrape
   include Capybara::DSL
+  
+  def go
+    (0..100).each do |number|
+      puts get_prisoner_by_din("12", "A", number.to_s.rjust(4, "0"))
+      sleep(30)
+    end
+  end
 
-  def get_data
+  # Gets a hash of prisoner info given an DIN.
+  # http://www.doccs.ny.gov/univinq/fpmsdoc.htm#din
+  def get_prisoner_by_din(year, letter, sequence)
     visit "/"
-    fill_in "M00_DIN_FLD1I", :with => "12"
-    fill_in "M00_DIN_FLD2I", :with => "A"
-    fill_in "M00_DIN_FLD3I", :with => "0001"
+    fill_in "M00_DIN_FLD1I", :with => year.to_s
+    fill_in "M00_DIN_FLD2I", :with => letter.to_s
+    fill_in "M00_DIN_FLD3I", :with => sequence.to_s
     click_button "Submit"    
     
-    puts page.find("#content").text
+    # Did we hit an error page?
+    errors = page.all("p.err")
+    if errors.size > 0
+      puts errors.map { |x| x.text }.join(", ")
+      return
+    end
+    
+    store = {} # A hash of data.
+
+    tables = page.find("#content").all("table")
+
+    # ID + Location info
+    store.merge!(Hash[tables[0].all("th").map { |x| x.text }.zip(tables[0].all("td").map { |x| x.text })])
+
+    # Sentence Terms and Release Dates
+    store.merge!(Hash[tables[2].all("th").map { |x| x.text }.zip(tables[2].all("td").map { |x| x.text })])
+
+    # Crimes of Conviction
+    crimes = { "crimes" => {} }
+    crimes["crimes"] = Hash[tables[1].all("td[headers=crime]").map { |x| x.text }.zip(tables[1].all("td[headers=class]").map { |x| x.text })]
+    store.merge!(crimes)  
+      
+    store
   end
 end
 
-Scrape.new.get_data
+Scrape.new.go
